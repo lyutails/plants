12bf07bf6c867c419ffd4833fc4a27f31f119069
lyutails <lyusya.cool@gmail.com>
2022-12-21 13:06:46 +0300

    feat: create burger lines

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './style.scss';
+
+function burgerCreate() {
+    const burger = document.querySelector('.burger');
+
+    for (let i = 1; i <= 4; i++) {
+        const burger_line = document.createElement('span');
+        burger_line.classList.add('burger_line');
+        burger.append(burger_line);
+    }
+}
+burgerCreate();
